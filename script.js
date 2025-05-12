fd5af978b1764edd4e7a024ff0edac2a98d2286b
Alice Kile <alice.kile@deliveryhero.com>
2025-05-12 13:34:06 +0200

    update canvas width/height logic

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 /* global fabric */
+
+const CANVAS_WIDTH = 700;
+const CANVAS_HEIGHT = 500;
+
 // Wait for the DOM to be fully loaded (good practice, though often works without)
 document.addEventListener("DOMContentLoaded", function () {
   // Initialize Fabric.js on the canvas element with id 'c'
   const canvas = new fabric.Canvas("c");
 
+  canvas.setWidth(CANVAS_WIDTH);
+  canvas.setHeight(CANVAS_HEIGHT);
+
   const demoImg = "https://fabricjs.github.io/assets/pug.jpg";
 
-  const radius = 300;
+  const radius = 200;
+
+  const group = new fabric.Group();
 
   const circle = new fabric.Circle({
     absolutePositioned: true,
     objectCaching: false,
     radius,
+    left: CANVAS_WIDTH / 2,
+    top: CANVAS_HEIGHT / 2,
     originX: "center",
     originY: "center",
   });
